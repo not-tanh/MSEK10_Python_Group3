93d06de92239a5c4502e112ec1f78de71a929040
giang123b <giang123b@gmail.com>
2022-10-10 12:02:29 +0700

    complete api get student list

---
+++ api/get_list_student_pagination.py
@@ -0,0 +1,30 @@
+import sqlite3
+import traceback
+
+from fastapi import APIRouter, HTTPException
+from fastapi_pagination import Page, add_pagination, paginate
+
+from student import Student
+from db import db_get_list_student as DBGetListStudent
+
+router = APIRouter()
+
+
+@router.get('/getliststudent')
+def getListStudent():
+    try:
+        listStudent = DBGetListStudent.getListStudent()
+        return listStudent
+    except sqlite3.IntegrityError:
+        raise HTTPException(status_code=400, detail='Student ID is not valid')
+    except:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail='Internal server error')
+
+
+@router.get('/students', response_model=Page[Student])
+async def get_users():
+    students = DBGetListStudent.getListStudent()
+    return paginate(students)
+
+
