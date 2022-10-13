76aed4a917baffb2889a617d18e43bec2538453c
Tuan Anh Nguyen <tanh@Tanhs-MacBook-Pro-15-inches.local>
2022-10-13 20:52:28 +0700

    Update gen data

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,5 @@
 import sys
 import traceback
-import uuid
 import random
 
 from faker import Faker
@@ -11,15 +10,19 @@ from db.db_create_student import insertStudent
 fake = Faker()
 
 try:
-    data = []
     for _ in range(int(sys.argv[1])):
-        student = Student(
-            **{'sid': str(uuid.uuid4()), 'first_name': fake.first_name(), 'last_name': fake.last_name(),
-               'email': fake.email(), 'dob': fake.date(), 'hometown': fake.city(),
-               'gpa': round(random.uniform(2, 4), 2)}
-        )
-        data.append(student)
-    insertStudent(data)
+        while True:
+            try:
+                student = Student(
+                    **{'sid': f'MSE{random.randint(1, 10000)}', 'first_name': fake.first_name(), 'last_name': fake.last_name(),
+                       'email': fake.email(), 'dob': fake.date(), 'hometown': fake.city(),
+                       'gpa': round(random.uniform(2, 4), 2)}
+                )
+                insertStudent(student)
+                break
+            except:
+                print('Duplicated ID. Try different ID')
+
 except:
     traceback.print_exc()
     print('Usage: python data_generator.py <data_count>')
